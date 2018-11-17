c99f1ae27852be0c25c986f101dcd3964c359f1e
Hasib Muhammad <hasibmu99@gmail.com>
2018-11-17 15:38:35 +0600

    dashboard route protected via route group of phroute

--- routes.php
+++ routes.php
@@ -1,4 +1,19 @@
 <?php
-$router->controller('/',App\Controllers\Frontend\HomeController::class);
-$router->controller('/users',App\Controllers\Frontend\UsersController::class);
-$router->controller('/dashboard',App\Controllers\Backend\DashboardController::class);
+
+use Phroute\Phroute\RouteCollector;
+
+$router->filter('auth', function () {
+    if (!isset($_SESSION['user'])) {
+        $errors[] = 'You are not logged in!';
+        $_SESSION['errors'] = $errors;
+        header("Location: /login");
+        exit();
+    }
+});
+
+$router->controller('/', App\Controllers\Frontend\HomeController::class);
+$router->controller('/users', App\Controllers\Frontend\UsersController::class);
+
+$router->group(['before' => 'auth'], function (RouteCollector $router) {
+    $router->controller('/dashboard', App\Controllers\Backend\DashboardController::class);
+});
